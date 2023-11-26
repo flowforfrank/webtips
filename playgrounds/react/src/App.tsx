9f254290dae8d43ffe5ace62c51d9357b26721dd
flowforfrank <ferenc@webtips.dev>
2023-11-26 18:42:59 +0100

    ✨ Add example for playgrounds

--- playgrounds/react/src/App.tsx
+++ playgrounds/react/src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react'
+
 const App = () => {
-  return (
-    <h1>React Playground</h1>
-  )
+    const [count, setCount] = useState(0)
+
+    return (
+        <main>
+            <h1>React Playground</h1>
+            <button onClick={() => setCount(count + 1)}>
+                Count: {count}
+            </button>
+        </main>
+    )
 }
 
 export default App
